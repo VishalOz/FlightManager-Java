55497d491e42d98aba7d2b8fe50891b4ced09e56
Vishal Oshada <vishaloshada@Vishals-MacBook-Air.local>
2025-05-18 17:13:31 +0530

    Passenger class is done

---
+++ Passenger.java
@@ -0,0 +1,23 @@
+public class Passenger {
+    private String passengerId;
+    private String name;
+    private String email;
+
+    public Passenger (String passengerId, String name, String email) {
+        this.passengerId = passengerId;
+        this.name = name;
+        this.email = email;
+    }
+
+    //getters
+    public String getPassengerId() {
+        return passengerId;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+}
